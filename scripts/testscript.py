7703777be2d8b6c59bdaadbac77560bc1ddbc4fe
Joo Suhong <rodney.joo@gmail.com>
2020-02-19 22:44:45 +0900

    Initial commit

---
+++ scripts/testscript.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup as bs
+
+
+def divider(left: int, right: int):
+    return left/right
+
+
+def bsobj_creator():
+    return bs
---
+++ test/test_testscript.py
@@ -0,0 +1,16 @@
+import unittest
+from scripts import testscript as ts
+from bs4 import BeautifulSoup as bs
+
+
+class DividerTest(unittest.TestCase):
+    def test_divider(self):
+        self.assertEqual(ts.divider(2, 2), 2/2)
+        self.assertEqual(type(ts.divider(2, 2)), float)
+        with self.assertRaises(ZeroDivisionError):
+            ts.divider(2, 0)
+
+
+class BsobjcreatorTest(unittest.TestCase):
+    def test_bsobjcreator(self):
+        self.assertEqual(type(ts.bsobj_creator()), bs)
